97ba238f5379d764702bb9848225016d27f52cfb
данило <playgroop@ukr.net>
2022-06-14 18:39:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Golowna'>
+        <header className='header'>
+          <div className='container'>
+            <h1>Союз Логистик</h1>
+            <nav>
+              <ul className="nav-item">
+                <li className="nav-list">
+                  Главная
+                </li>
+                <li className="nav-list">
+                  О компании
+                </li>
+                <li className="nav-list">
+                  Отзывы
+                </li>
+                <li className="nav-list">
+                  Контакты
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
